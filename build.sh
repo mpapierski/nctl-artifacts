fe99ca6b2f1a12eb67dc3a613e9a414a9e5a394b
Michał Papierski <michal@papierski.net>
2023-04-20 14:43:56 +0200

    v1.4.14

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ BASE_URL="http://nctl.casperlabs.io.s3-website.us-east-2.amazonaws.com"
 REMOTE_FILES=(
     "add_bid.wasm"
     "casper-node"
+    "global-state-update-gen"
     "chainspec.toml.in"
     "config.toml"
     "delegate.wasm"
@@ -16,6 +17,7 @@ REMOTE_FILES=(
 )
 
 VERSIONS=(
+    "v1.4.14"
     "v1.4.5"
     "v1.3.0"
 )
@@ -31,8 +33,10 @@ for VERSION in "${VERSIONS[@]}";
 do
     mkdir -p ${ARTIFACTS_DIR}/${VERSION}
     pushd $VERSION
-    cargo build --release -p casper-node
+    # cargo build --release -p casper-node
     cp ./target/release/casper-node ${ARTIFACTS_DIR}/${VERSION}/
+    cp ./target/release/global-state-update-gen ${ARTIFACTS_DIR}/${VERSION}/
+    # cargo build --release -p global-state-update-gen
     popd
 done
 
@@ -42,12 +46,24 @@ for REMOTE_FILE in "${REMOTE_FILES[@]}";
 do
     for VERSION in "${VERSIONS[@]}";
     do
-        if [ "$REMOTE_FILE" = 'casper-node' ]; then
+        if [ "$REMOTE_FILE" = 'casper-node' ];
+        then
             CASPER_NODE="${ARTIFACTS_DIR}/${VERSION}/casper-node"
             echo "$VERSION: Copying compiled artifact from $CASPER_NODE..."
 
             if [ -f "$CASPER_NODE" ]; then
                 $CASPER_NODE --version
+            else
+                echo "$VERSION: casper-node binary was not properly built"
+                exit 1
+            fi
+        elif [ "$REMOTE_FILE" = 'global-state-update-gen' ];
+        then
+            GLOBAL_STATE_UPDATE_GEN="${ARTIFACTS_DIR}/${VERSION}/global-state-update-gen"
+            echo "$VERSION: Copying compiled artifact from $GLOBAL_STATE_UPDATE_GEN..."
+
+            if [ -f "$GLOBAL_STATE_UPDATE_GEN" ]; then
+                $GLOBAL_STATE_UPDATE_GEN --version
             else
                 echo "$VERSION: casper-node binary was not properly built"
                 exit 1
